8d7c454892ba838e316a8ec1e3939bb890815e0e
Lucas <lucas.ramos46@fatec.sp.gov.br>
2025-09-25 18:52:35 -0300

    feat(p1): cria componente principal e faz hello

---
+++ p1-gerenciamento-de-lembretes/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <div className="container border rounded my-2 p-3">
+    Hello, Lembretes
+  </div>
+}
+
+export default App
