a4abab0cb45277fabd6504fb6e8ff7d3245b1205
Lucas <lucas.ramos46@fatec.sp.gov.br>
2025-09-26 13:39:01 -0300

    feat(p1): remove lembretes

--- p1-gerenciamento-de-lembretes/src/App.jsx
+++ p1-gerenciamento-de-lembretes/src/App.jsx
@@ -1,48 +1,57 @@
+import React from 'react'
 import LembreteEntrada from './LembreteEntrada'
 import LembreteLista from './LembreteLista'
 
-const App = () => {
-  const lembretes = [
-    {
-      id:1,
-      descricao: 'Preparar aula de programação',
-      favorito: false,
-      icone:'solid'
-    },
-    {
-      id:2,
-      descricao: 'Fazer feira',
-      favorito: false,
-      icone:'regular'
-    },
-        {
-      id:3,
-      descricao: 'Preparar Marmita',
-      favorito: false,
-      icone:'regular'
-    }
-  ]
+class App extends React.Component {
 
-  return ( 
-    <div className="container border rounded my-2 p-3">
-      <div className="row">
-        {
-          lembretes.map((lembrete) => (
-            <div className="col-12">
-              <LembreteLista 
-                descricao={lembrete.descricao}
-                icone={lembrete.icone} />
-            </div>
+  state = {
+    lembretes: []
+  }
+  removerLembrete = (id) => {
+    const atualizarLembrete = this.state.lembretes.filter((lembrete) => lembrete.id !== id)
+    this.setState({ lembretes: atualizarLembrete})
+  }
 
-            
-            
-          ))
-        }
-
+  componentDidMount() {
+    const lembretesFicticios = [
+      {
+        id: 1,
+        descricao: 'Preparar aula de programação',
+        favorito: true
+      },
+      {
+        id: 2,
+        descricao: 'Fazer feira',
+        favorito: false
+      },
+      {
+        id: 3,
+        descricao: 'Preparar Marmita',
+        favorito: false
+      }
+    ]
+    this.setState({lembretes: lembretesFicticios })
+  }
+  render() {
+    return (
+      <div className="container border rounded my-2 p-3">
+        <div className="row">
+          {
+            this.state.lembretes.map((lembrete) => (
+              <div className="col-12">
+                <LembreteLista
+                  descricao={lembrete.descricao}
+                  favorito={lembrete.favorito} 
+                  remover={ () => this.removerLembrete(lembrete.id)}/>
+              </div>
+            ))
+          }
+        </div>
+        <LembreteEntrada />
       </div>
-      <LembreteEntrada />
-    </div>
-  )
+    )
+
+  }
 }
 
 export default App
--- p1-gerenciamento-de-lembretes/src/LembreteLista.jsx
+++ p1-gerenciamento-de-lembretes/src/LembreteLista.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const LembreteLista = (props) => {
+export class LembreteLista extends Component {
+  render() {
     return (
-        <div className="card">
+              <div className="card">
             <div className="card-body d-flex">
                 <div className="flex-grow-1 ms-2 border p-1">
-                    <h4 className='text-center'>{props.descricao}</h4>
+                    <h4 className='text-center'>{this.props.descricao}</h4>
                 </div>
                 <div className="p-2">
                     <button className='btn btn-outline-primary w-100 mt-2'>
-                        <i className={`fa-${props.icone} fa-star`}></i>
+                        <i className={`fa-${this.props.favorito? 'solid' : 'regular'} fa-star`}></i>
                         
                     </button>
                 </div>
                 <div className="p-2">
-                    <button className='btn btn-outline-primary w-100 mt-2'>
+                    <button 
+                        className='btn btn-outline-primary w-100 mt-2'
+                        onClick={this.props.remover}  
+                    >
                         <i className={'fa-solid fa-trash'}></i>
                     </button>
                 </div>
             </div>
         </div>
-
     )
+  }
 }
 
 export default LembreteLista
